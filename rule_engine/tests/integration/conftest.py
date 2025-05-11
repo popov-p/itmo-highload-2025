ad6284b07034f7d81dccc5bf78fbf74ee7a707e4
Pavel Popov <pashafizmat1@gmail.com>
2025-05-11 22:34:51 +0300

    test input fix for integration

--- rule_engine/tests/integration/conftest.py
+++ rule_engine/tests/integration/conftest.py
@@ -16,7 +16,7 @@ def fixed_batch():
         gpu_info=gpu_info,
         ker_temp=90.0,
         ker_load=60.0,
-        mem_temp=80.0,
+        mem_temp=112.0,
         mem_load=66.33,
         timestamp=str(time.time()),
     )
@@ -30,7 +30,7 @@ def make_batch():
             gpu_id=gpu_id,
             model="RTX 3090",
             max_ker_temp=int(60.0),
-            max_mem_temp=int(115.0),
+            max_mem_temp=int(50.0),
         )
 
         batch = Batch(

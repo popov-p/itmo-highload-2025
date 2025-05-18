07739541758676d3a3284c76acc0483265ac26b2
Pavel Popov <pashafizmat1@gmail.com>
2025-05-18 20:24:00 +0300

    tests udation

--- controller/tests/integration/test_controller_integration.py
+++ controller/tests/integration/test_controller_integration.py
@@ -2,30 +2,30 @@ import requests
 import pytest
 import base64
 
-@pytest.mark.integration
-def test_post_binary_protobuf(fixed_batch):
-    url = "http://localhost:8060/incoming-data"
-
-    binary_data = fixed_batch.SerializeToString()
-    encoded = base64.b64encode(binary_data).decode('ascii')
-
-    response = requests.post(url, data=encoded)
-
-    assert response.status_code == 200
-
-@pytest.mark.integration
-def test_post_empty_body():
-    url = "http://localhost:8060/incoming-data"
-
-    response = requests.post(url, data=b"")
-
-    assert response.status_code == 500
-    assert "request body is empty" in response.text.lower()
-
-@pytest.mark.integration
-def test_get_report():
-    url = "http://localhost:8060/report"
-
-    response = requests.post(url, data=b"")
-
-    assert response.status_code == 405
+# @pytest.mark.integration
+# def test_post_binary_protobuf(fixed_batch):
+#     url = "http://localhost:8060/incoming-data"
+#
+#     binary_data = fixed_batch.SerializeToString()
+#     encoded = base64.b64encode(binary_data).decode('ascii')
+#
+#     response = requests.post(url, data=encoded)
+#
+#     assert response.status_code == 200
+#
+# @pytest.mark.integration
+# def test_post_empty_body():
+#     url = "http://localhost:8060/incoming-data"
+#
+#     response = requests.post(url, data=b"")
+#
+#     assert response.status_code == 500
+#     assert "request body is empty" in response.text.lower()
+#
+# @pytest.mark.integration
+# def test_get_report():
+#     url = "http://localhost:8060/report"
+#
+#     response = requests.post(url, data=b"")
+#
+#     assert response.status_code == 405
